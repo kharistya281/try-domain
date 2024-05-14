4ba6e7c8700ada401fb8524fec02834ee9a8dd37
Kharisma Agustya Zahra Salsabilla <142194971+kharistya281@users.noreply.github.com>
2024-05-14 10:26:53 +0700

    Update main.py

--- try-domain-aw/main.py
+++ try-domain-aw/main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import mysql.connector
+import pandas as pd 
+import psycopg2
 
 def connectToDatabase():
     try:
@@ -15,7 +17,21 @@ def connectToDatabase():
     except Exception as e:
         st.error(f"Error connecting to database: {e}")
         return None
-    
+
+def run_query(query):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    df = pd.DataFrame(data, columns=columns)
+    cursor.close()
+    conn.close()
+    return df
+
+
+
+
 def main():
     st.title("Aplikasi Streamlit untuk Akses Database MySQL")
 
@@ -23,6 +39,7 @@ def main():
         conn = connectToDatabase()
         if conn:
             st.success("Connected to database successfully!")
+    query = st.text_area('Enter your SQL query here:', )
 
 if __name__ == "__main__":
     main()
